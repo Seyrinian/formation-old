4984c250c84444806b886e912e5d1f46e0c78704
vfourny-prest <vfourny-prest@redoute.fr>
2021-10-08 14:18:55 +0200

    add corrections to static

--- components/TPBlock.vue
+++ components/TPBlock.vue
@@ -2,6 +2,16 @@
   <v-container id="tp-list" fluid>
     <v-row justify="center">
       <v-col cols="10">
+        <v-row justify="center"
+          ><v-col cols="4"
+            ><v-btn
+              class="ma-2"
+              href="https://classroom.github.com/a/0IjSWJk4"
+              x-large
+              ><v-icon left> mdi-github </v-icon> Dépôt de remise des TP
+            </v-btn></v-col
+          ></v-row>
+        <v-divider></v-divider>
         <v-row>
           <v-col> <h1>Travaux pratiques</h1></v-col>
         </v-row>
@@ -53,16 +63,40 @@
           </v-col>
         </v-row>
         <v-divider></v-divider>
-        <v-row justify="center"
-          ><v-col cols="4"
-            ><v-btn
+         <v-row>
+          <v-col> <h1>Corrections</h1></v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn
               class="ma-2"
-              href="https://classroom.github.com/a/0IjSWJk4"
-              x-large
-              ><v-icon left> mdi-github </v-icon> Dépôt de remise des TP
-            </v-btn></v-col
-          ></v-row
-        >
+              href="/vacation/corrections/TP0.zip"
+              download
+              large
+            >
+              <v-icon left> mdi-folder-zip </v-icon>
+              Introduction: Exercices de découverte
+            </v-btn>
+            <v-btn
+              class="ma-2"
+              href="/vacation/corrections/TP1.zip"
+              download
+              large
+            >
+              <v-icon left> mdi-folder-zip </v-icon>
+              TP1: Manipulation de tableaux et de formulaires
+            </v-btn>
+            <v-btn
+              class="ma-2"
+              href="/vacation/corrections/TP2.zip"
+              download
+              large
+            >
+              <v-icon left> mdi-folder-zip </v-icon>
+              TP2: Fonctions en PHP
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -71,7 +105,6 @@
 <style lang="scss">
 @import '../assets/sass/custom-variables.scss';
 #tp-list {
-  height: 100vh;
   background-color: $--php-color-3;
 }
 </style>
